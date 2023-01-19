c2c2b0e49543e01b732fe94627787648f4544cb0
TomatoFT <20520926@gm.uit.edu.vn>
2023-01-19 22:06:20 +0700

    Add demo

--- model/transformer_cnn.py
+++ model/transformer_cnn.py
@@ -88,39 +88,6 @@ def load_captions_data(filename):
         return caption_mapping, text_data
 
 
-# def train_val_split(caption_data, train_size=0.8, shuffle=True):
-#     """Split the captioning dataset into train and validation sets.
-
-#     Args:
-#         caption_data (dict): Dictionary containing the mapped caption data
-#         train_size (float): Fraction of all the full dataset to use as training data
-#         shuffle (bool): Whether to shuffle the dataset before splitting
-
-#     Returns:
-#         Traning and validation datasets as two separated dicts
-#     """
-
-#     # 1. Get the list of all image names
-#     all_images = list(caption_data.keys())
-
-#     # 2. Shuffle if necessary
-#     if shuffle:
-#         np.random.shuffle(all_images)
-
-#     # 3. Split into training and validation sets
-#     train_size = int(len(caption_data) * train_size)
-
-#     training_data = {
-#         img_name: caption_data[img_name] for img_name in all_images[:train_size]
-#     }
-#     validation_data = {
-#         img_name: caption_data[img_name] for img_name in all_images[train_size:]
-#     }
-
-#     # 4. Return the splits
-#     return training_data, validation_data
-
-
 # Load the dataset
 captions_mapping, text_data = load_captions_data("model/Flickr8k.token.txt")
 
