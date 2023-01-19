29a441d0369d37f064db653cc1d32c37c67648d5
TomatoFT <20520926@gm.uit.edu.vn>
2023-01-19 20:00:18 +0700

    remove something

--- download_data.sh
+++
@@ -1,9 +0,0 @@
-wget -q https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip
-
-wget -q https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip
-
-unzip -qq Flickr8k_Dataset.zip
-
-unzip -qq Flickr8k_text.zip
-
-rm Flickr8k_Dataset.zip Flickr8k_text.zip
